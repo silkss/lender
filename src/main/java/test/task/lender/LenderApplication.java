2b54c3116859b13c4cf28618328a23cba468a5fb
Mikhail <k.21480@gmail.com>
2025-07-25 17:13:31 +0300

    Init commit

---
+++ src/main/java/test/task/lender/LenderApplication.java
@@ -0,0 +1,13 @@
+package test.task.lender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenderApplication.class, args);
+	}
+
+}
